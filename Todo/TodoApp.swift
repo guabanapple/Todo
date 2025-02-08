42edc67c51a695f39487b15be50d9ba6f04b4ca1
guabanapple <nakasujihiroki@gmail.com>
2025-02-08 17:43:05 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 中筋裕樹 on 2025/02/08.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by 中筋裕樹 on 2025/02/08.
+//
+
+import Testing
+@testable import Todo
+
+struct TodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
